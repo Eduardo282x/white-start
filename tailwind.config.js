ccd6c2f544b7ba6c71a487694ba9c3a77f53c5f9
Eduardo <eduardokunmixyeah@gmail.com>
2023-12-05 20:41:14 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+import * as nextui from '@nextui-org/react'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{html,js}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [nextui()],
+}
+
