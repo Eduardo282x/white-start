c6898e9e717a4d3b342685de615a5aa8f2ac86ee
Eduardo282x <eduardokunmixyeah@gmail.com>
2023-12-07 15:46:14 -0400

    change heig

--- src/components/Shared/Card/CardShared.jsx
+++ src/components/Shared/Card/CardShared.jsx
@@ -1,13 +1,13 @@
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import PropTypes from "prop-types";
 import './card.css'
 
 export const CardShared = ({width, height}) => {
     return (
-            <Card sx={{ minWidth: {width}, minHeight: 200 }}>
+            <Card sx={{ minWidth: {width}, minHeight: {height} }} className='cardContent'>
                 <CardContent>
                     <Typography variant="h5" component="div">
                         Title
@@ -27,3 +27,9 @@ export const CardShared = ({width, height}) => {
             </Card>
     )
 }
+
+
+CardShared.propTypes = {
+    width: PropTypes.number,
+    height: PropTypes.number,
+};
